ecbfe79d495b610650f3d4b7cf96d6d3cc4b2000
piry9200 <ymt104ro09@gmail.com>
2024-01-18 14:57:26 +0900

    add: ログ機能を追加

--- static/scripts/pomodoro.js
+++ static/scripts/pomodoro.js
@@ -5,10 +5,12 @@ const rest_form_second = document.getElementById("rest_form_second");
 const start_stop_button = document.getElementById("start_stop_button");
 const reset_button = document.getElementById("reset_button");
 const mode_change_button = document.getElementById("mode_change_button");
+const clear_log_button = document.getElementById("clear_log_button");
 const display_minute =  document.getElementById("display_minute");
 const display_second =  document.getElementById("display_second");
 const display_status =  document.getElementById("display_status");
 const apply_form_buttons = document.getElementsByClassName("apply_form");
+const log_box = document.getElementById("log_box")
 
 class Timer {
     constructor(min, sec, working, timerId, posing, must_change){
@@ -25,6 +27,11 @@ class Timer {
             this.sec = this.sec - 1;
         }else{ //秒が0のときに実行
             if(this.min == 0){ //00:00のときの処理
+                if(this.working){
+                    log.add_log(get_date() + " に作業["+ work_form_minute.value + ":" + work_form_second.value + "]を完了");
+                }else{
+                    log.add_log(get_date() + " に休憩["+ rest_form_minute.value + ":" + rest_form_second.value + "]を完了");
+                }
                 this.mode_change();
             }else{
                 this.min = this.min - 1;
@@ -81,34 +88,40 @@ function handleTimer(){
 }
 
 function handle_clicked_start_stop_button(){
-    if(timer.working && !timer.posing && timer.must_change){ //集中開始前に押された場合
-        timer.working = true; //集中モードに
+    if(timer.working && !timer.posing && timer.must_change){ //集中開始前に押された場合(作業開始のタイミング)
+        timer.working = true; //集中モードに変更
         timer.must_change = false;
         timer.inquire_form(); //timerにformの時間をセット
         timer.timerId = setInterval("handleTimer()", 1000);
         start_stop_button.textContent = "ストップ";
-    }else if(timer.working && !timer.posing && !timer.must_change){ //集中モード中に押された場合
+        log.add_log(get_date() + " に作業["+ work_form_minute.value + ":" + work_form_second.value + "]を開始");
+    }else if(timer.working && !timer.posing && !timer.must_change){ //集中モード中に押された場合(作業一時停止のタイミング)
         clearInterval(timer.timerId);
         timer.posing = true;
         start_stop_button.textContent = "スタート";
-    }else if(timer.working && timer.posing && !timer.must_change){ //集中モード中の一時停止中に押された場合
+        log.add_log(get_date() + " に作業["+ work_form_minute.value + ":" + work_form_second.value + "]を一時停止");
+    }else if(timer.working && timer.posing && !timer.must_change){ //集中モード中の一時停止中に押された場合(作業再開のタイミング)
         timer.posing = false;
         timer.timerId = setInterval("handleTimer()", 1000);
         start_stop_button.textContent = "ストップ";
-    }else if(!timer.working && !timer.posing && timer.must_change){ //休憩モード開始前に押された場合
-        timer.working = false; //休憩モードに
+        log.add_log(get_date() + " に作業["+ work_form_minute.value + ":" + work_form_second.value + "]を再開");
+    }else if(!timer.working && !timer.posing && timer.must_change){ //休憩モード開始前に押された場合(休憩開始のタイミング)
+        timer.working = false; //休憩モードに変更
         timer.must_change = false;
         timer.inquire_form(); //timerにformの時間をセット
         timer.timerId = setInterval("handleTimer()", 1000);
         start_stop_button.textContent = "ストップ";
-    }else if(!timer.working && !timer.posing && !timer.must_change){ //休憩モード中に押された場合
+        log.add_log(get_date() + " に休憩["+ rest_form_minute.value + ":" + rest_form_second.value + "]開始");
+    }else if(!timer.working && !timer.posing && !timer.must_change){ //休憩モード中に押された場合(休憩一時停止のタイミング)
         clearInterval(timer.timerId);
         timer.posing = true;
         start_stop_button.textContent = "スタート";
-    }else if(!timer.working && timer.posing && !timer.must_change){ //休憩モード中の一時停止中に押された場合
+        log.add_log(get_date() + " に休憩["+ rest_form_minute.value + ":" + rest_form_second.value + "]を一時停止");
+    }else if(!timer.working && timer.posing && !timer.must_change){ //休憩モード中の一時停止中に押された場合(休憩再開のタイミング)
         timer.posing = false;
         timer.timerId = setInterval("handleTimer()", 1000);
         start_stop_button.textContent = "ストップ";
+        log.add_log(get_date() + " に休憩["+ rest_form_minute.value + ":" + rest_form_second.value + "]を再開");
     }
 }
 
@@ -116,6 +129,7 @@ function handle_clicked_reset_button(){
     timer.must_change = true;
     timer.posing = false;
     start_stop_button.textContent = "スタート";
+    log.add_log(get_date() + " にタイマーをリセット");
     clearInterval(timer.timerId);
     timer.inquire_form();
     timer.update_display()
@@ -123,6 +137,12 @@ function handle_clicked_reset_button(){
 
 function handle_clicked_mode_change_button(){ // 集中モード、休憩モードの開始時または、一時停止中に実行できる
     if( (timer.working && !timer.posing && timer.must_change) || (!timer.working && !timer.posing && timer.must_change) || (timer.working && timer.posing && !timer.must_change) || (!timer.working && timer.posing && !timer.must_change) ){
+        if(timer.working){
+            log.add_log(get_date() + " に作業から休憩へチェンジ");
+        }else{
+            log.add_log(get_date() + " に休憩から作業へチェンジ");
+        }
+        
         timer.mode_change();
 
     }
@@ -149,6 +169,45 @@ function change_background_color(){
     }
 }
 
+class Log{
+    constructor(log_data){
+        //web側のlogboxには、log_dataの文字列が表示される
+        this.log_data = log_data;
+    }
+    
+    //web側の表示をlog_dataの値で上書きする
+    update_display(){
+        log_box.innerText = this.log_data;
+        console.log("nekotest")
+        log_box.scroll(0,log_box.scrollHeight);
+    }
+
+    //web側、システム側の両方にログを追加する
+    add_log(log_content){
+        this.log_data += log_content + "\n" ;
+        this.update_display();
+    }
+
+    //web側、システム側両方から完全にログを削除する
+    clear_log(){
+        this.log_data = "";
+        this.update_display();
+    }
+}
+
+function handle_clicked_clear_log_button(){ // ログを削除する
+    log.clear_log()
+}
+
+function get_date(){
+    var date = new Date();
+    return date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' 
+    +('0' + date.getDate()).slice(-2) + ' ' +  ('0' + date.getHours()).slice(-2) + ':' + 
+    ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
+}
+
+// ------------------↑↑↑↑ 関数やクラスの定義　↑↑↑↑↑-----------------------
+
 //　イベントを仕込む
 start_stop_button.addEventListener("click", handle_clicked_start_stop_button);
 reset_button.addEventListener("click", handle_clicked_reset_button);
@@ -156,10 +215,17 @@ mode_change_button.addEventListener("click", handle_clicked_mode_change_button);
 for(let i=0; i<2; i++){
     apply_form_buttons[i].addEventListener("click", handle_clicked_apply_button);
 }
+clear_log_button.addEventListener("click", handle_clicked_clear_log_button);
 
-// timerの初期設定
+
+// インスタンスtimerの初期設定
 const init_min = work_form_minute.value
 const init_sec = work_form_second.value
 const timer = new Timer(init_min, init_sec, 10, null, false, true);
-//タイマー表示の初期設定
-timer.update_display()
+//web側のタイマー表示の初期設定
+timer.update_display();
+
+// logの初期設定
+//const log = new Log(サーバから送られてきた保存記録); 
+const log = new Log("");
+
