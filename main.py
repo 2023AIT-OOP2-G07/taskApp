7c2e967b06a71a4d771e84a567b26b9577aba68b
arrrai <153758193+arrrai@users.noreply.github.com>
2024-01-18 15:23:55 +0900

    memo.py,html,mainを変えた

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from modules.memo import memo_blueprint
 from flask import Flask
 app = Flask(__name__)
-# app.register_blueprint(memo_blueprint)
+app.register_blueprint(memo_blueprint)
 
 if __name__ == '__main__':
-    app.run()
+    try:
+        app.run(debug=True)
+    except Exception as e:
+        print(f"An error occurred: {e}")
--- modules/memo.py
+++ modules/memo.py
@@ -1,31 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for, Blueprint
 
-from flask import Flask, render_template, request, redirect, url_for
+memo_blueprint = Blueprint("memo", __name__)
 
+# Flaskアプリケーションの作成
 app = Flask(__name__)
 
 # メモを格納するリスト
-memos = []
+app.config['memos'] = []
 
 
-@app.route('/')
+@memo_blueprint.route('/')
 def index():
-    return render_template('memo.html', memos=memos)
+    return render_template('memo.html', memos=app.config['memos'])
 
 
-@app.route('/add_memo', methods=['POST'])
+@memo_blueprint.route('/add_memo', methods=['POST'])
 def add_memo():
     # フォームからのデータを取得
     new_memo = request.form.get('memo_text')
 
     # メモを追加
-    memos.append(new_memo)
+    app.config['memos'].append(new_memo)
 
     # メモ一覧ページにリダイレクト
-    return redirect(url_for('index'))
+    return redirect(url_for('memo.index'))
+
+# BlueprintをFlaskアプリケーションに登録
+app.register_blueprint(memo_blueprint)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
 
 
 
