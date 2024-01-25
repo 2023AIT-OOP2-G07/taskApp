9c678451f3651142bdffdc29b3f22d9b7461b7a9
veryverylongdog <annmasako51@gmail.com>
2024-01-25 16:28:31 +0900

    todo仕様変更

--- modules/todo.py
+++ modules/todo.py
@@ -1,98 +1,96 @@
-from flask import Flask, Blueprint, render_template, request, redirect, url_for, jsonify,current_app
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, jsonify, current_app
 import os
+import json
 
 # Flask Blueprintの作成
 todo_bp = Blueprint('todo', __name__)
 
-# SQLAlchemyの初期化
-db = SQLAlchemy()
+# データファイルのパス
+data_file_path = 'todo_data.json'
 
-# Taskクラスの定義
-class Task(db.Model):
-    __tablename__ = "tasks"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    text = db.Column(db.Text())
-    status = db.Column(db.Integer)
+# データを読み込む関数
+def load_data():
+    if os.path.exists(data_file_path):
+        with open(data_file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    else:
+        return []
 
-# データベーステーブルを作成する関数
-def create_tables():
-    with current_app.app_context():
-        db.create_all()
-
-# ファイルにデータを保存する関数
-def save_to_file(file_path, data):
-    with open(file_path, 'w') as file:
-        file.write(data)
+# データを保存する関数
+def save_data(data):
+    with open(data_file_path, 'w') as file:
+        json.dump(data, file)
 
 # ホームページのルート
 @todo_bp.route('/todo')
 def todo():
-    tasks = Task.query.all()
+    tasks = load_data()
     return render_template('todo.html', tasks=tasks)
 
 # 新しいタスクの追加ルート
 @todo_bp.route('/new', methods=["POST"])
 def new():
-    task = Task()
-    task.text = request.form["new_text"]
-    task.status = 0
-    db.session.add(task)
-    db.session.commit()
-    return redirect(url_for('todo.index'))
+    data = load_data()
+    new_task = {
+        'id': len(data) + 1,
+        'text': request.form["new_text"],
+        'status': 0
+    }
+    data.append(new_task)
+    save_data(data)
+    return redirect(url_for('todo.todo'))
 
 # 完了処理ルート
 @todo_bp.route('/complete', methods=["POST"])
 def complete():
-    id = request.form["id"]
-    task = Task.query.filter_by(id=id).first()
-    task.status = 1
-    db.session.commit()
-    return redirect(url_for('todo.index'))
+    data = load_data()
+    id = int(request.form["id"])
+    for task in data:
+        if task['id'] == id:
+            task['status'] = 1
+            break
+    save_data(data)
+    return redirect(url_for('todo.todo'))
 
 # 更新処理ルート
 @todo_bp.route('/update', methods=["POST"])
 def update():
-    id = request.form["id"]
+    data = load_data()
+    id = int(request.form["id"])
     text = request.form["text"]
-    task = Task.query.filter_by(id=id).first()
-    task.text = text
-    db.session.commit()
-    return redirect(url_for('todo.index'))
+    for task in data:
+        if task['id'] == id:
+            task['text'] = text
+            break
+    save_data(data)
+    return redirect(url_for('todo.todo'))
 
 # 削除処理ルート
 @todo_bp.route('/delete', methods=["POST"])
 def delete():
-    id = request.form["id"]
-    task = Task.query.filter_by(id=id).first()
-    db.session.delete(task)
-    db.session.commit()
-    return redirect(url_for('todo.index'))
+    data = load_data()
+    id = int(request.form["id"])
+    data = [task for task in data if task['id'] != id]
+    save_data(data)
+    return redirect(url_for('todo.todo'))
 
 # APIエンドポイントのルート
 @todo_bp.route('/api/get_data', methods=["POST"])
 def get_data():
-    tasks = Task.query.all()
-    task_list = [{'id': task.id, 'text': task.text, 'status': task.status} for task in tasks]
-    return jsonify(task_list)
+    tasks = load_data()
+    return jsonify(tasks)
 
 # FlaskアプリケーションをBlueprintで初期化する関数
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.sqlite'
-    db.init_app(app)
 
     # BlueprintをFlaskアプリケーションに登録
     app.register_blueprint(todo_bp, url_prefix='/todo')
 
-
-    with app.app_context():
-        create_tables()
-
     return app
 
-# app = Flask(__name__) の行を削除
 # create_app 関数を使ってアプリケーションオブジェクトを作成
 app = create_app()
 
